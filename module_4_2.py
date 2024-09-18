4fbcfb354e15ff2288b990b9589f94c8a82509e4
sseerr1 <46776271+sseerr1@users.noreply.github.com>
2024-09-18 17:22:37 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+
+
+test_function()
+#inner_function() Выдайт ошибку: NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
